5c94cd5ccc0492887662d6381ba4402f8709570c
Prashat Sharma <prahshatsharma@csgroupchd.com>
2026-02-16 16:14:13 +0530

    appliaiton api modifed

--- src/chat/schemas/message.schema.ts
+++ src/chat/schemas/message.schema.ts
@@ -4,23 +4,35 @@ import { Document, Types } from 'mongoose';
 @Schema({ timestamps: true })
 export class Message extends Document {
 
+  // conversation id
   @Prop({ type: Types.ObjectId, ref: 'conversations', required: true })
   conversationId: Types.ObjectId;
 
+  // application id (correct ref)
+  @Prop({ type: Types.ObjectId, ref: 'applications', required: true })
+  applicationId: Types.ObjectId;
+
+  // sender
   @Prop({ type: Types.ObjectId, required: true })
   senderId: Types.ObjectId;
 
   @Prop({ enum: ['admin','user','system'], required: true })
   senderType: string;
 
+  // message content
   @Prop()
   message: string;
 
-  @Prop({ default: 'text' })
+  @Prop({ default: 'text' }) // text | file | image
   messageType: string;
 
+  // seen/unseen
   @Prop({ default: false })
   isRead: boolean;
+
+  // optional: read time
+  @Prop()
+  readAt: Date;
 }
 
 export const MessageSchema = SchemaFactory.createForClass(Message);
